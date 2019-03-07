531f5efb5b018ceded4b2416eea79b2f365522be
Kevin von Spiczak <caevv@me.com>
2019-03-07 21:35:02 +0100

    Main.gd

--- Main.gd
+++ Main.gd
@@ -6,29 +6,24 @@ Responsibilities:
 -relay messages between sibling children
 
 """
-
-
 extends Node
 
 # Declare member variables here. Examples:
-onready var global = get_node("/root/global")
-onready var BGMusic = get_node("BGMusic")
-var Music_Last_Playback_Position : float = 0.0
+signal Dialog_box_completed(box_name)
 
-onready var PauseLabel = get_node("GUIContainer/FullRect/PauseLabel")
-
-var DialogBox = preload("res://GUI/DialogBox.tscn")
-onready var DialogBoxContainer = get_node("GUIContainer/FullRect/CenterContainer/DialogBoxes")
+onready var bg_music : AudioStreamPlayer = $BGMusic
+onready var dialog_box_container : Control = $GUIContainer/FullRect/CenterContainer/DialogBoxes
+onready var level_container : Node2D = $LevelContainer
+onready var pause_label : Label = $GUIContainer/FullRect/PauseLabel
+onready var dialog_box : PackedScene = preload("res://GUI/DialogBox.tscn")
 
 enum States { initializing, playing, paused }
-var State = States.initializing
 
-onready var LevelContainer = $LevelContainer
-var LevelScenes = [ "res://Levels/Level1.tscn", "res://Levels/Level2.tscn" ]
-var Level_Num : int = -1
-var Current_Level_Node
-
-signal DialogBox_completed(box_name)
+var _state : int = States.initializing setget _set_state
+var music_last_playback_position : float = 0.0
+var level_scenes : Array = [ "res://Levels/Level1.tscn", "res://Levels/Level2.tscn" ]
+var level_num : int = -1
+var current_level_node : Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -36,51 +31,46 @@ func _ready():
 	global.set_main(self)
 	transition("fade-in")
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
-func set_state(state):
-	State = state
+func _set_state(new_state):
+	_state = new_state
+	# could check here whether or not the state was actually changed,
+	# and then react on state change (emit signal e.g.)
 
-func spawn_dialog_box(boxTitle : String, textArray : Array, requestedBy):
-	var newDialogBox = DialogBox.instance()
-	DialogBoxContainer.add_child(newDialogBox)
-	newDialogBox.start(boxTitle, textArray, self, requestedBy)
+func spawn_dialog_box(boxTitle : String, textArray : Array, requested_by):
+	var new_dialog_box = dialog_box.instance()
+	dialog_box_container.add_child(new_dialog_box)
+	new_dialog_box.start(boxTitle, textArray, self, requested_by)
 		# note: the order is important. Must add_child() before you can start()
 
 func show_pause_menu():
 	find_node("PauseMenu").show()
-	PauseLabel.set_text("PAUSED. Press esc to resume.")
+	pause_label.set_text("PAUSED. Press esc to resume.")
 	
-	Music_Last_Playback_Position = BGMusic.get_playback_position()
-	BGMusic.stop()
+	music_last_playback_position = bg_music.get_playback_position()
+	bg_music.stop()
 
-#	BGMusic.set_stream_paused(true)
+#	bg_music.set_stream_paused(true)
 	global.pause_game()
 
 func hide_pause_menu():
 	find_node("PauseMenu").hide()
 	global.resume_game()
-	PauseLabel.set_text("Press esc to Pause Game.")
-#	BGMusic.set_stream_paused(false)
-	BGMusic.play()
-	BGMusic.seek(Music_Last_Playback_Position)
+	pause_label.set_text("Press esc to Pause Game.")
+#	bg_music.set_stream_paused(false)
+	bg_music.play()
+	bg_music.seek(music_last_playback_position)
 
 func start_game():
-	set_state(States.playing)
-	$BGMusic.play()
+	self._state = States.playing
+	bg_music.play()
 	start_next_level()
-
 
 func transition(animation_name):
 	$AnimationPlayer.play(animation_name)
-
-
-	
-	
-
 
 func start_next_level():
 	# Need to add fade out and fade in
@@ -88,34 +78,34 @@ func start_next_level():
 	transition("fade-out")
 	yield(get_tree().create_timer(0.5), "timeout")
 	
-	if Current_Level_Node != null:
-		if Current_Level_Node.has_method("end"):
-			Current_Level_Node.end() # levels should free themselves
+	if current_level_node != null:
+		if current_level_node.has_method("end"):
+			current_level_node.end() # levels should free themselves
 		else:
-			Current_Level_Node.queue_free()
+			current_level_node.queue_free()
 		
-	Level_Num = wrapi(Level_Num+1, 0, LevelScenes.size())
+	level_num = wrapi(level_num+1, 0, level_scenes.size())
 	
-	var current_level_scene = load(LevelScenes[Level_Num])
-	Current_Level_Node = current_level_scene.instance()
-	LevelContainer.add_child(Current_Level_Node)
+	var current_level_scene = load(level_scenes[level_num])
+	current_level_node = current_level_scene.instance()
+	level_container.add_child(current_level_node)
 	
-	if Current_Level_Node.has_method("start"):
-		Current_Level_Node.start()
+	if current_level_node.has_method("start"):
+		current_level_node.start()
 
 	transition("fade-in")
 	yield(get_tree().create_timer(0.5), "timeout")
 		
 func _input(event):
-	if State == States.playing:
+	if _state == States.playing:
 		if event.is_action_pressed("pause"):
 			show_pause_menu()
-			set_state(States.paused)
+			_set_state(States.paused)
 			global.pause_game()
-	elif State == States.paused:
+	elif _state == States.paused:
 		if event.is_action_pressed("pause"):
 			hide_pause_menu()
-			set_state(States.playing)
+			_set_state(States.playing)
 			global.resume_game()
 	
 	if event.is_action_pressed("next_level"):
@@ -127,7 +117,7 @@ func _on_DialogBox_completed(dialog_box_title, requesting_node):
 
 	# other nodes may need dialog boxes, they'll ask Main for one, then expect a callback
 	elif requesting_node != self:
-		var _err = connect("DialogBox_completed", requesting_node, "_on_BialogBox_completed")
+		var _err = connect("Dialog_box_completed", requesting_node, "_on_DialogBox_completed")
 		if _err: push_warning(_err)
-		emit_signal("DialogBox_completed", dialog_box_title)
-		disconnect("DialogBox_completed", requesting_node, "_on_BialogBox_completed")
+		emit_signal("Dialog_box_completed", dialog_box_title)
+		disconnect("Dialog_box_completed", requesting_node, "_on_DialogBox_completed")
