9dfa1bca02daaaf9c6b73863c833a5ac83b1151f
plexsoup <plexsoup@gmail.com>
2019-03-07 20:23:21 -0800

    Fixed bug in audio sliders
    Was caused by wrong signal destination in inspector. I removed the inspector signals and connect the signals in code.

--- GUI/VolSlider.gd
+++ GUI/VolSlider.gd
@@ -7,11 +7,13 @@ export var Bus : int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+
+	# creating the signal connection in code, because its too easy to have bugs from
+	# inspector signals that didn't get hooked up right.
 	var _err = connect("value_changed", self, "_on_VolSlider_value_changed")
 	if _err: push_warning(_err)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+
 
 
 func _on_VolSlider_value_changed(value):
