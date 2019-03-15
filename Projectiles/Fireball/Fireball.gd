16b061deab8084f45a1b5d28d038528b651b394a
Kevin von Spiczak <caevv@me.com>
2019-03-15 23:19:05 +0100

    fix enemy fireball -> player collision

--- Projectiles/Fireball/Fireball.gd
+++ Projectiles/Fireball/Fireball.gd
@@ -9,7 +9,8 @@ export var damage : int = 1
 var velocity : Vector2 = Vector2.ZERO
 
 func _ready():
-	connect("body_entered", self, "_on_body_entered")
+	connect("area_entered", self, "_on_area_entered")
+#	connect("body_entered", self, "_on_body_entered")
 
 func _process(delta):
 	position += velocity * delta
@@ -26,7 +27,15 @@ func shoot(bullet_position : Vector2, rot : float, initial_velocity : Vector2):
 		var sfx_player : AudioStreamPlayer2D = sfx.get_child(rnd_sfx)
 		sfx_player.play()
 
-func _on_body_entered(body : PhysicsBody2D):
-	if body == global.player:
-		connect("hit", body, "_on_hit")
-		emit_signal("hit", damage)
+func _on_area_entered(area : Area2D):
+	if is_instance_valid(area.get_parent()):
+		if area.get_parent() == global.player:
+			connect("hit", area.get_parent(), "_on_hit")
+			emit_signal("hit", damage)
+
+#
+#func _on_body_entered(body : PhysicsBody2D):
+#	print(body)
+#	if body == global.player:
+#		connect("hit", body, "_on_hit")
+#		emit_signal("hit", damage)
