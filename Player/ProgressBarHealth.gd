3af409dc5ab17d2e41d05128f18c05ad98aae09d
plexsoup <plexsoup@gmail.com>
2019-03-16 14:34:39 -0700

    Texture Health Bar failed in exported project
    Changed it to a basic progress bar

---
+++ Player/ProgressBarHealth.gd
@@ -0,0 +1,14 @@
+extends ProgressBar
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var player = get_parent().get_parent()
+	value = float(player.health) / float(player.max_health) * 100.0
